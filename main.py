1e4ff860c04ff526541b26219b0f500bba3318fd
Dongrammar <123712077+Dongrammar@users.noreply.github.com>
2023-06-04 11:02:43 +0800

    更新 main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -2,19 +2,30 @@ def on_forever():
     if mbit_输入类.rocker(AnalogPin.P2,
         AnalogPin.P1,
         DigitalPin.P0,
-        mbit_输入类.enRocker.PRESS):
-        basic.show_icon(IconNames.SCISSORS)
-        basic.pause(100)
+        mbit_输入类.enRocker.UP):
+        turtle.forward(1)
     if mbit_输入类.rocker(AnalogPin.P2,
         AnalogPin.P1,
         DigitalPin.P0,
-        mbit_输入类.enRocker.UP):
-        basic.show_leds("""
-            . # # . #
-                        # # # # #
-                        # # # # #
-                        . . . # #
-                        . . . # #
-        """)
-        basic.pause(100)
+        mbit_输入类.enRocker.DOWN):
+        turtle.back(1)
+    if mbit_输入类.rocker(AnalogPin.P2,
+        AnalogPin.P1,
+        DigitalPin.P0,
+        mbit_输入类.enRocker.LEFT):
+        turtle.turn_left()
+        turtle.forward(1)
+        turtle.turn_right()
+    if mbit_输入类.rocker(AnalogPin.P2,
+        AnalogPin.P1,
+        DigitalPin.P0,
+        mbit_输入类.enRocker.RIGHT):
+        turtle.turn_right()
+        turtle.forward(1)
+        turtle.turn_left()
+    if mbit_输入类.rocker(AnalogPin.P2,
+        AnalogPin.P1,
+        DigitalPin.P0,
+        mbit_输入类.enRocker.PRESS):
+        pass
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+function reboot () {
+    basic.clearScreen()
+    control.reset()
+}
 basic.forever(function () {
     if (mbit_输入类.Rocker(
     AnalogPin.P2,
@@ -41,6 +45,21 @@ basic.forever(function () {
     DigitalPin.P0,
     mbit_输入类.enRocker.Press
     )) {
-        turtle.home()
+        basic.showString("Show Mode")
+        turtle.setSpeed(1000)
+        turtle.forward(10)
+        turtle.turnRight()
+        turtle.forward(3)
+        turtle.turnRight()
+        turtle.back(2)
+        basic.pause(2000)
+        turtle.setSpeed(25)
+        turtle.turnLeft()
+        turtle.setSpeed(50)
+        turtle.forward(100)
+        reboot()
     }
+})
+control.inBackground(function () {
+    music.playMelody("E B C5 A B G A F ", 120)
 })
